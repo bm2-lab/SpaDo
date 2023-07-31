c89c7a8860d27e24e8050447ec636ddc388ce5db
BinDuan <bioinfo_db@163.com>
2023-08-01 06:47:00 +0800

    Update SpaDo.r

--- R/SpaDo.r
+++ R/SpaDo.r
@@ -391,7 +391,7 @@ DomainHclust<-function (distribution_distance, autoselection = TRUE, auto_resolu
     cluster_hc_all = cutree(result_hc, k = 1:cluster_hc_dy_num)
     cluster_hc_all_order <- cluster_hc_all[result_hc$order, ]
     cluster_hc_all_order_rename <- as.data.frame(cluster_hc_all_order)
-    colnames(cluster_hc_all_order_rename) <- paste("SO_level", 
+    colnames(cluster_hc_all_order_rename) <- paste("Domain_level", 
         1:cluster_hc_dy_num, sep = "_")
     cluster_hc_all_order_rename_c <- apply(cluster_hc_all_order_rename, 
         2, function(x) {
@@ -400,7 +400,7 @@ DomainHclust<-function (distribution_distance, autoselection = TRUE, auto_resolu
     row.names(cluster_hc_all_order_rename_c) <- row.names(cluster_hc_all_order_rename)
     cluster_hc_all_order_rename_c <- as.data.frame(cluster_hc_all_order_rename_c)
     for (i in 1:ncol(cluster_hc_all_order_rename_c)) {
-        cluster_hc_all_order_rename_c[, i] <- paste("Term", i, cluster_hc_all_order_rename_c[, i], sep = "_")
+        cluster_hc_all_order_rename_c[, i] <- paste("Domain", i, cluster_hc_all_order_rename_c[, i], sep = "_")
     }
     return(list(hclust_result_df = cluster_hc_all_order_rename_c, 
         hclust_result_model = result_hc, hclust_result_order = cluster_hc_all_order))
