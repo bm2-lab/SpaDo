9cdc9a441774d380c9878f654780f928c1d6f396
BinDuan <bioinfo_db@163.com>
2024-08-08 15:37:44 +0800

    Update SpaDo.r

--- R/SpaDo.r
+++ R/SpaDo.r
@@ -363,11 +363,11 @@ DistributionDistance <- function(cell_type_distribution,distance = c("JSD", "man
         propor_dis <- as.matrix(propor_dis)
         return(propor_dis)
     }else if(method_choose=="JSD"){
-        if(n_cores<1){
+        if(no_cores<1){
             print("n_core cannot be less than 1!")
             break
         }
-        if(n_cores==1){
+        if(no_cores==1){
             require(philentropy)
             propor_dis <- philentropy::distance(cell_type_distribution, method = method_choose)
             row.names(propor_dis) <- row.names(cell_type_distribution)
